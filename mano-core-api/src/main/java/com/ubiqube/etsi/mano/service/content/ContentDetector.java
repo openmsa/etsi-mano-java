7ab71730a36dffd6fba9d6f164110fece2484944
Olivier Vignaud <ovi@ubiqube.com>
2024-12-30 19:26:33 +0100

    💡 core: Add comment on ContentDetector.

--- mano-core-api/src/main/java/com/ubiqube/etsi/mano/service/content/ContentDetector.java
+++ mano-core-api/src/main/java/com/ubiqube/etsi/mano/service/content/ContentDetector.java
@@ -6,5 +6,11 @@ import com.ubiqube.etsi.mano.repository.ManoResource;
 
 public interface ContentDetector {
 
+	/**
+	 * Determines the media type of the given ManoResource.
+	 *
+	 * @param mr the ManoResource for which the media type is to be determined
+	 * @return the MediaType of the given ManoResource
+	 */
 	MediaType getMediaType(ManoResource mr);
 }

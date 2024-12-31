263e62a4959261883ccce611fb192f1ba46d18cd
Olivier Vignaud <ovi@ubiqube.com>
2024-12-31 13:10:53 +0100

    🚑️ core: Fix compilation problem in InterfaceInfo.

--- mano-core-api/src/main/java/com/ubiqube/etsi/mano/service/InterfaceInfoService.java
+++ mano-core-api/src/main/java/com/ubiqube/etsi/mano/service/InterfaceInfoService.java
@@ -1,18 +1,18 @@
 /**
- *     Copyright (C) 2019-2024 Ubiqube.
+ * Copyright (C) 2019-2024 Ubiqube.
  *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
  *
- *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see https://www.gnu.org/licenses/.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
  */
 package com.ubiqube.etsi.mano.service;
 
@@ -84,7 +84,7 @@ public class InterfaceInfoService {
 		cmii.setSecurityInfo(createSecInfo());
 		cmii.setStandardVersion(x.version().toString());
 		cmii.setProviderSpecificApiVersion(x.version().toString());
-		final List<SupportedOperations> ope = createSupportedOperation(x.lst());
+		final List<SupportedOperations> ope = createSupportedOperation(x.methods());
 		cmii.setSupportedOperations(ope);
 		cmii.setType(x.part());
 		return cmii;
@@ -93,7 +93,7 @@ public class InterfaceInfoService {
 	private static List<SupportedOperations> createSupportedOperation(final List<HttpMethod> lst) {
 		return lst.stream().map(x -> {
 			final SupportedOperations so = new SupportedOperations();
-			so.setOperationName(x.action() + x.payh());
+			so.setOperationName(x.action() + x.path());
 			return so;
 		}).toList();
 	}

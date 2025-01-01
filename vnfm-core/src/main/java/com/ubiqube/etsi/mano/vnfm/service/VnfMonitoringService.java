834566813a4272d399e0a59b3f63da0772298f47
Olivier Vignaud <ovi@ubiqube.com>
2025-01-01 13:09:55 +0100

    🎨 vnfm-core: Add missing repository services.

--- vnfm-core/src/main/java/com/ubiqube/etsi/mano/vnfm/service/VnfMonitoringService.java
+++ vnfm-core/src/main/java/com/ubiqube/etsi/mano/vnfm/service/VnfMonitoringService.java
@@ -1,18 +1,18 @@
 /**
- *     Copyright (C) 2019-2024 Ubiqube.
+ * Copyright (C) 2019-2024 Ubiqube.
  *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
  *
- *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see https://www.gnu.org/licenses/.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
  */
 package com.ubiqube.etsi.mano.vnfm.service;
 
@@ -28,7 +28,7 @@ import com.ubiqube.etsi.mano.dao.mano.MonitoringParams;
 import com.ubiqube.etsi.mano.dao.mano.pm.PmJob;
 import com.ubiqube.etsi.mano.dao.mano.pm.PmJobCriteria;
 import com.ubiqube.etsi.mano.dao.mano.vim.VimConnectionInformation;
-import com.ubiqube.etsi.mano.jpa.PmJobsJpa;
+import com.ubiqube.etsi.mano.vnfm.service.repository.PmJobsRepositoryService;
 
 /**
  *
@@ -39,9 +39,9 @@ import com.ubiqube.etsi.mano.jpa.PmJobsJpa;
 public class VnfMonitoringService {
 
 	private static final Logger LOG = LoggerFactory.getLogger(VnfMonitoringService.class);
-	private final PmJobsJpa pmJobsJpa;
+	private final PmJobsRepositoryService pmJobsJpa;
 
-	public VnfMonitoringService(final PmJobsJpa pmJobsJpa) {
+	public VnfMonitoringService(final PmJobsRepositoryService pmJobsJpa) {
 		this.pmJobsJpa = pmJobsJpa;
 	}
 
--- vnfm-core/src/test/java/com/ubiqube/etsi/mano/vnfm/service/VnfMonitoringServiceTest.java
+++ vnfm-core/src/test/java/com/ubiqube/etsi/mano/vnfm/service/VnfMonitoringServiceTest.java
@@ -1,18 +1,18 @@
 /**
- *     Copyright (C) 2019-2024 Ubiqube.
+ * Copyright (C) 2019-2024 Ubiqube.
  *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
  *
- *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see https://www.gnu.org/licenses/.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
  */
 package com.ubiqube.etsi.mano.vnfm.service;
 
@@ -30,12 +30,12 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.ubiqube.etsi.mano.dao.mano.MonitoringParams;
 import com.ubiqube.etsi.mano.dao.mano.pm.PmJob;
-import com.ubiqube.etsi.mano.jpa.PmJobsJpa;
+import com.ubiqube.etsi.mano.vnfm.service.repository.PmJobsRepositoryService;
 
 @ExtendWith(MockitoExtension.class)
 class VnfMonitoringServiceTest {
 	@Mock
-	private PmJobsJpa pmJpa;
+	private PmJobsRepositoryService pmJpa;
 
 	@Test
 	void test() {
